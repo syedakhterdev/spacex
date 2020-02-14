c497d2e116f7adb060cc2c2e0aae3e5cc4a71223
Syed Akhter <dev.syed.akhter@gmail.com>
2020-02-14 11:42:52 -0500

    Init app with empty home component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
